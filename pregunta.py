b1be2a7ca3680b38da20fea6260c9e957f4fbc54
jaforeroto <112499361+jaforeroto@users.noreply.github.com>
2022-10-12 15:17:51 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -17,7 +17,10 @@ def clean_data():
     df.dropna(inplace=True)
     df.columns.values
 
+    # sexo
     df['sexo']= df['sexo'].str.lower() 
+
+    # tipo emprendimiento
     df['tipo_de_emprendimiento']= df['tipo_de_emprendimiento'].str.lower()
 
     # idea_negocio
@@ -31,22 +34,21 @@ def clean_data():
     df['barrio']=df['barrio'].str.replace("-","_")
     df['barrio']=df['barrio'].str.replace("_"," ")
 
-    #línea_credito
+    # línea_credito
     df['línea_credito']= df['línea_credito'].str.lower()
     df['línea_credito']=df['línea_credito'].str.replace("-","_")
     df['línea_credito']=df['línea_credito'].str.replace("_"," ")
     df['línea_credito']=df['línea_credito'].str.replace(".","")
     df['línea_credito']=df['línea_credito'].str.strip()
 
-    #fecha_de_beneficio
+    # fecha_de_beneficio
     df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,dayfirst=True)
-    
-    #monto_del_credito
+
+    # monto_del_credito
     df['monto_del_credito']=df['monto_del_credito'].str.replace(",","")
     df['monto_del_credito']=df['monto_del_credito'].str.replace("$","")
     df['monto_del_credito']=df['monto_del_credito'].str.strip()
     df['monto_del_credito'] = df['monto_del_credito'].apply(pd.to_numeric, downcast="integer", errors='ignore')
-    df.drop_duplicates(inplace=True)
-    df.dropna(inplace=True)
+
 
     return df
