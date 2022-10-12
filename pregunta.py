dacff1d164e610005674cada369537cf18436dfa
jaforeroto <112499361+jaforeroto@users.noreply.github.com>
2022-10-12 14:49:20 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -15,7 +15,8 @@ def clean_data():
     # Convertir textos en minuscula
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df=df.dropna() 
+    df=df.dropna()
+    df.columns.values
     df['sexo'] = df['sexo'].str.lower()
     df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
     df['idea_negocio'] = df['idea_negocio'].str.lower()
@@ -24,6 +25,9 @@ def clean_data():
 
     # idea de negocio
     df['idea_negocio'] = df['idea_negocio'].str.strip()
+    df['idea_negocio']= df['idea_negocio'].str.replace("-","_") 
+    df['idea_negocio']=df['idea_negocio'].str
+
 
 
 
