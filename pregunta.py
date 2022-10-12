546c5bfade3e290a17ad0d7c7ae064d70a835759
jaforeroto <112499361+jaforeroto@users.noreply.github.com>
2022-10-12 14:35:25 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -15,7 +15,11 @@ def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
     df=df.dropna() 
     df['sexo'] = df['sexo'].str.lower()
-    
+    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
+    df['idea_negocio'] = df['idea_negocio'].str.lower()
+    df['barrio'] = df['barrio'].str.lower()
+    df['línea_credito'] = df['línea_credito'].str.lower()
+
 
 
 
