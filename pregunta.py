09904ca2463f3af6e852af217a9a60cf38c686b0
jaforeroto <112499361+jaforeroto@users.noreply.github.com>
2022-10-12 15:03:09 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -10,36 +10,49 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 import pandas as pd
 
 
-def clean_data():
+import pandas as pd
 
-    # Convertir textos en minuscula
+
+def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
     df=df.dropna()
     df.columns.values
 
-    df['sexo'] = df['sexo'].str.lower()
-    df['tipo_de_emprendimiento'] = df['tipo_de_emprendimiento'].str.lower()
-    df['idea_negocio'] = df['idea_negocio'].str.lower()
-    df['barrio'] = df['barrio'].str.lower()
-    df['línea_credito'] = df['línea_credito'].str.lower()
+    # Sexo
+    df['sexo']= df['sexo'].str.lower() 
+    df['tipo_de_emprendimiento']= df['tipo_de_emprendimiento'].str.lower()
 
-    # idea de negocio
+    # idea_negocio
+    df['idea_negocio']= df['idea_negocio'].str.lower()
     df['idea_negocio']=df['idea_negocio'].str.replace("-","_")
     df['idea_negocio']=df['idea_negocio'].str.replace("_"," ")
     df['idea_negocio']=df['idea_negocio'].str.strip()
 
     # Barrio
+    df['barrio']= df['barrio'].str.lower()
     df['barrio']=df['barrio'].str.replace("-","_")
     df['barrio']=df['barrio'].str.replace("_"," ")
     df['barrio']=df['barrio'].str.replace(".","")
     df['barrio']=df['barrio'].str.replace("bel¿n","belen")
     df['barrio']=df['barrio'].str.replace("antonio nari¿o","antonio nariño")
     df['barrio']=df['barrio'].str.strip()
+    
+    #línea_credito
+    df['línea_credito']= df['línea_credito'].str.lower()
+    df['línea_credito']=df['línea_credito'].str.replace("-","_")
+    df['línea_credito']=df['línea_credito'].str.replace("_"," ")
+    df['línea_credito']=df['línea_credito'].str.replace(".","")
+    df['línea_credito']=df['línea_credito'].str.replace("soli diaria","solidaria")
+    df['línea_credito']=df['línea_credito'].str.strip()
 
+    #fecha_de_beneficio
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,dayfirst=True)
 
-
-
-
+    #monto_del_credito
+    df['monto_del_credito']=df['monto_del_credito'].str.replace(",","")
+    df['monto_del_credito']=df['monto_del_credito'].str.replace("$","")
+    df['monto_del_credito']=df['monto_del_credito'].str.strip()
+    df['monto_del_credito'] = df['monto_del_credito'].apply(pd.to_numeric, downcast="integer", errors='ignore')
 
     return df
